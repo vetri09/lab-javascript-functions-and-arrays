1c87d18e7d2087a943bb16919e827de3a885cd3f
Vetrivelan <vetri0907.vv@gmail.com>
2021-06-10 14:11:42 +0530

    first commit

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,18 +1,98 @@
 // Progression #1: Greatest of the two numbers
-
+let greatestOfTwoNumbers = (num1, num2) => {
+  return greaterNumber = num1 > num2 ? num1 : num2 > num1 ? num2 : num1;
+}
 // Progression #2: The lengthy word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
-
+let findScaryWord = (words) =>
+{
+  let value = 0, index = 0, lengthWord = "";
+  if(words.length == 0)
+  {
+    return null;
+  }
+  else 
+  {
+    for(let i = 0; i< words.length; i++)
+    {
+      if(words[i].length > value)
+      {
+        value = words[i].length;
+        index = i;
+      }
+      lengthWord = words[index];
+    }
+  }
+  return lengthWord;
+}
 // Progression #3: Net Price
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-
+let netPrice = (numbers) => {
+  let total = 0;
+  numbers.forEach(element => {
+    total += element;
+  });
+  return total;
+}
+//bonus round #3.1
+const mixedArr = [63, 122, 'audi', 61, true, 'volvo', '20', 'lamborghini', 38, 156];
+let add = (mixedArr) => {
+  let total = 0;
+  if(mixedArr.length == 0)
+  {
+    return 0;
+  }
+  else
+  {
+    for(let i=0; i<mixedArr.length; i++)
+    {
+      if(typeof(mixedArr[i])=="string"){
+        mixedArr[i] = mixedArr[i].length;
+      }
+      if(typeof(mixedArr[i])=="object"){
+        //I DONT GET THIS, NOT WORKING
+        throw "its an object/array";
+      }
+      total += mixedArr[i];
+    }
+  }
+  return total;
+}
 // Progression #4: Calculate the average
+
 // Progression 4.1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
-
+function midPointOfLevels(numbersAvg)
+{
+  let value = netPrice(numbersAvg);
+  let totalNums = numbersAvg.length;
+  return numbersAvg.length == 0 ? null : value/totalNums;
+}
 // Progression 4.2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
-
+let averageWordLength = (wordsArr) => {
+  let total = 0;
+  wordsArr.forEach(element => {
+    let value = element.length;
+    total += value;
+  });
+  return wordsArr.length == 0 ? null : total/wordsArr.length;
+}
+//4.3 mixed array
+function avg (mixedArr) {
+  let total = 0;
+    mixedArr.forEach(element => {
+      if(typeof(element)=="string")
+      {
+          total += element.length;
+      }
+        else    
+        {
+            total += element;
+        }
+    });
+    return mixedArr.length == 0 ? null : total/mixedArr.length;
+}
 // Progression #5: Unique arrays
 const wordsUnique = [
   'bread',
@@ -31,7 +111,19 @@ const wordsUnique = [
 
 // Progression #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
-
+function searchElement(wordsFind, value){
+  for(let i=0; i<wordsFind.length; i++)
+  {
+    if(wordsFind.length==0)
+    {
+      return null;
+    }
+    else if(wordsFind[i]==value)
+    {
+      return true;
+    }
+  }
+}
 // Progression #7: Count repetition
 const wordsCount = [
   'machine',
@@ -46,7 +138,17 @@ const wordsCount = [
   'disobedience',
   'matter'
 ];
-
+function howManyTimesElementRepeated(wordsCount, value)
+{
+  let num = 0;
+  wordsCount.forEach(element => {
+    if(element==value)
+    {
+      num++;
+    }
+  })
+  return num;
+}
 // Progression #8: Bonus
 
 const matrix = [
